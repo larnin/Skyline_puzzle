e2e45f1d1590dc1659f01296a2bb102c48dbe523
Nicolas <nicolas-donald.laurent@laposte.net>
2017-05-16 15:06:29 +0200

    Base Physics

--- Assets/Scripts/G.cs
+++ Assets/Scripts/G.cs
@@ -7,6 +7,7 @@ using UnityEngine;
 public sealed class G
 {
     private static volatile G _instance;
+    private Player _player;
 
     public static G Sys
     {
@@ -15,6 +16,17 @@ public sealed class G
             if (G._instance == null)
                 G._instance = new G();
             return G._instance;
+        }
+    }
+
+    public Player player
+    {
+        get { return _player; }
+        set
+        {
+            if (_player != null)
+                DebugConsole.Log("2 Players instanciated !");
+            _player = value;
         }
     }
 }
---
+++ Assets/Scripts/GyroGravity.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GyroGravity : MonoBehaviour
+{
+    public float multiplier = 1;
+
+    void Start()
+    {
+        Input.gyro.enabled = true;
+    }
+
+
+    void Update ()
+    {
+        var g = (Input.gyro.gravity + new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0)) * multiplier;
+        Physics.gravity = g;
+        Physics2D.gravity = new Vector2(g.x, g.y);
+	}
+}
