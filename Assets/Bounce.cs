8ecbb22c361ea83c722e6c75ba3bb9a2bf8d0512
Unknown <julesgassie@gmail.com>
2017-05-16 17:05:28 +0200

    CubePhysic Bouncer

---
+++ Assets/Bounce.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bounce : MonoBehaviour {
+
+    public float bounceStrenght;
+
+    void OnCollisionEnter(Collision _hit)
+    {
+        if (_hit.gameObject.tag.Contains("Player"))
+        {
+            ContactPoint contact = _hit.contacts[0];
+            _hit.rigidbody.AddForce(contact.normal * -1 * bounceStrenght * _hit.relativeVelocity.magnitude);
+        }
+    }
+}
