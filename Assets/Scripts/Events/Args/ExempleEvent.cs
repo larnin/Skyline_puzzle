f77645ba6a23d104ba4a02b1c171b8082c26573d
Nicolas <nicolas-donald.laurent@laposte.net>
2017-05-16 14:18:08 +0200

    Add base scripts

---
+++ Assets/Scripts/Events/Args/ExempleEvent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class ExempleEvent : EventArgs {
+
+	public ExempleEvent (string _title, int _reward)
+	{
+		Title = _title;
+		Reward = _reward;
+	}
+
+	public string Title;
+	public int Reward;
+}
---
+++ Assets/Scripts/G.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public sealed class G
+{
+    private static volatile G _instance;
+
+    public static G Sys
+    {
+        get
+        {
+            if (G._instance == null)
+                G._instance = new G();
+            return G._instance;
+        }
+    }
+}
