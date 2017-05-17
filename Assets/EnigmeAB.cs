4878bbfaf7528134543ac0571d76405b66afb4ae
ETU\j.lorans <juliensud@live.fr>
2017-05-17 17:30:39 +0200

    LD JULIEN

---
+++ Assets/EnigmeAB.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnigmeAB : ActivatedElement {
+
+    GameObject[] lesA;
+    GameObject[] lesB;
+
+    bool turned = true;
+
+
+    // Use this for initialization
+    void Start () {
+        lesA = GameObject.FindGameObjectsWithTag("A");
+        lesB = GameObject.FindGameObjectsWithTag("B");
+
+
+        foreach(GameObject e in lesB)
+        {
+            e.SetActive(false);
+        }
+    }
+
+    public override void Activate()
+    {
+
+        foreach (GameObject e in lesB)
+        {
+            e.SetActive(turned);
+        }
+
+        foreach (GameObject e in lesA)
+        {
+            e.SetActive(!turned);
+        }
+
+        turned = !turned;
+        
+    }
+
+}
--- Assets/Scripts/LDElements/Trigger.cs
+++ Assets/Scripts/LDElements/Trigger.cs
@@ -5,4 +5,13 @@ using UnityEngine;
 public class Trigger : MonoBehaviour {
 
     public GameObject ActivatedElement;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("WormholeIn"))
+        {
+            transform.position = other.gameObject.GetComponent<WormholeIn>().WormholeOut.transform.position;
+        }
+    }
 }
